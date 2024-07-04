e1525881d62fa7205ef91418e1f0989b849a75b3
HBhawe <harshalb97@outlook.com>
2024-07-04 16:33:31 +0200

    Completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let text = string;
+  let number = num;
+  let resultString = "";
 
+  for (let i = 1; i <= number; i++) {
+    resultString += text;
+  }
+  return resultString;
 };
 
 // Do not edit below this line
