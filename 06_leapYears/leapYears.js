1c3f21ee1c6d93e3ecd97499619e63bfe03435e9
Harshal Bhawe <harshalb97@outlook.com>
2024-07-07 17:41:59 +0200

    Completed exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function (yearToCheck) {
+  let year = yearToCheck;
 
+  if (year % 4 == 0) {
+    if (year % 400 == 0 && year % 100 == 0) {
+      return true;
+    } else if (year % 100 != 0) {
+      return true;
+    } else return false;
+  } else return false;
 };
 
 // Do not edit below this line
