08bc19b8cd4ed96e8a8f3c96ff7c3851e5657e2b
Harshal Bhawe <harshalb97@outlook.com>
2024-07-07 17:25:09 +0200

    Updated solution 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  let argumentsLength = arguments.length;
+  var array = arguments[0];
+  //   var length = array.length;
+
+  for (let i = 0; i < argumentsLength; i++) {
+    let element = arguments[i];
+
+    array = array.filter((item) => item !== element);
+
+    // OLD METHOD: not working for test 3 onwards
+    // let index = array.indexOf(element);
+    // if (index > -1) {
+    //   // only splice array when item is found
+    //   array.splice(index, 1); // 2nd parameter means remove one item only
+    // }
+  }
+  return array;
 };
 
 // Do not edit below this line
