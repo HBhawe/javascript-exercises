59b2fdc91a0bdfd7298fe5892d1b2bb12a8e32c4
Harshal Bhawe <harshalb97@outlook.com>
2024-07-07 17:52:55 +0200

    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperatureFahrenheit) {
+  let temperature = temperatureFahrenheit;
+  let temperatureCelsius = parseFloat(
+    (((temperature - 32) * 5) / 9).toFixed(1)
+  );
+  return temperatureCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperatureCelsius) {
+  let temperature = temperatureCelsius;
+  let temperatureFahrenheit = parseFloat((temperature * 1.8 + 32).toFixed(1));
+  return temperatureFahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
