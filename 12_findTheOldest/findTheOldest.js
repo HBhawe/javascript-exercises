c39d63a53313dde8945cc07d8539d49863909384
HBhawe <harshalb97@outlook.com>
2024-07-10 15:43:45 +0200

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  const oldest = arr.sort(function (a, b) {
+    if (a.yearOfDeath === undefined) {
+      let year = new Date().getFullYear();
+      a.yearOfDeath = year;
+    } else if (b.yearOfDeath === undefined) {
+      let year = new Date().getFullYear();
+      b.yearOfDeath = year;
+    }
+    const lastPerson = a.yearOfDeath - a.yearOfBirth;
+    const nextPerson = b.yearOfDeath - b.yearOfBirth;
+    return lastPerson > nextPerson ? -1 : 1;
+  });
 
+  return oldest[0];
 };
 
 // Do not edit below this line
