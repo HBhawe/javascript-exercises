d9c8692cb156922c3c02e5e6649a71cd92b9d78c
HBhawe <harshalb97@outlook.com>
2024-07-10 15:18:34 +0200

    Completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function (arr) {
+  let titleArray = new Array();
 
+  let array = arr;
+  let length = array.length;
+
+  for (let index = 0; index < length; index++) {
+    let book = array[index];
+    titleArray.push(book.title);
+  }
+  return titleArray;
 };
 
 // Do not edit below this line
