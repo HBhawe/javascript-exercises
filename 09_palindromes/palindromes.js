440cbb02c0c60e9ce4fab77e4a8cc65e9e7ea390
HBhawe <harshalb97@outlook.com>
2024-07-10 14:51:22 +0200

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // regular expression for special characters
+  var regExp = /[\W_]/g;
 
+  //   convert to lower case and replace special characters with empty string
+  //   this is done to accomodate special characters and uppercase
+  //   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Regular_expressions/Character_class_escape
+  var lowerCaseRegExp = str.toLowerCase().replace(regExp, "");
+
+  //   split the string into an array of characters, reverse it and join
+  var reverseString = lowerCaseRegExp.split("").reverse().join("");
+  return reverseString === lowerCaseRegExp;
 };
 
 // Do not edit below this line
